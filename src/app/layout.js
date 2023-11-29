9dec035d28f6b923194ac6c59022f24ba75d59e7
datodato788 <gogiawenom8@gmail.com>
2023-11-29 17:58:38 +0400

    Adding routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,12 @@ export const metadata = {
 export default function        RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Navbar/>
+      <body className={inter.className}>
+        <div className="container">
+        <Navbar/>
       {children} 
     <Footer/> 
-       
+    </div>
        </body>
       
     </html>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,49 +1,53 @@
-import Links from "react"
-import styles from './navbar.module.css'
+import React from "react";
+import styles from "./navbar.module.css";
+import Link from "next/link";
 
 function Navbar() {
-   
-  const links=[
+  const links = [
     {
       id: 1,
       title: "Home",
-
+      url: '/'
     },
     {
       id: 2,
       title: "Portfolio",
+      url: '/portfolio'
     },
     {
       id: 3,
       title: "Blog",
+      url: '/blog'
     },
     {
       id: 4,
       title: "About",
+      url: '/about'
     },
     {
       id: 5,
       title: "Contact",
+      url: '/contact'
     },
     {
       id: 6,
       title: "Dashboard",
+      url: '/dashboard'
     },
   ];
+
   return (
-         <div className={styles.container}>  
-           <div>Main</div>
-                         
-         <div className={styles.links}>
-          {links.map((link) => (<div key={link.title}></div>
-          ))}
-         </div>
-        </div>
+    <div className={styles.container}>
+      <div>Main</div>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link href={link.url}>{link.title}</Link>
+        ))}
+      </div>
+    </div>
   );
 }
-         export default Navbar 
 
-
-
+export default Navbar;
 
          
