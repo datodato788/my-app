557460b4aa8feeb0d0858262a6c9a191fd60560e
datodato788 <gogiawenom8@gmail.com>
2023-12-20 14:42:12 +0400

    pt1

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider className={Styles.theme}>
           <div className="container">
             <Navbar />
             {children}
